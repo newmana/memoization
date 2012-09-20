a47c064f08f2b939b626f14369ab7de443df20ba
Andrew Newman <andrewfnewman@gmail.com>
2012-09-21 09:00:10 +1000

    Got 3 versions working

--- haskell/hylotree.hs
+++ haskell/hylotree.hs
@@ -1,4 +1,6 @@
-data Tree a = Leaf a | Node [Tree a]
+import qualified Data.Monoid as M
+
+data Tree a = Leaf a | Node [Tree a] deriving (Show, Eq)
 
 fold :: (a -> b) -> ([b] -> b) -> Tree a -> b
 fold f g (Leaf x) = f x
@@ -8,7 +10,7 @@ unfold :: (b -> Bool) -> (b -> a) -> (b -> [b]) -> b -> Tree a
 unfold p v h x = if p x then Leaf (v x) else
 	Node (map (unfold p v h) (h x))
 
-data LTree a = LLeaf a | LNode a [LTree a]
+data LTree a = LLeaf a | LNode a [LTree a] deriving (Show, Eq)
 
 fill :: (a -> b) -> ([b] -> b) -> Tree a -> LTree b
 fill f g = fold (lleaf f) (lnode g)
@@ -30,10 +32,32 @@ single x
   | length x == 1 = True
   | otherwise = False
 
+okSingle1 = single [1] == True
+okSingle2 = single [] == False
+okSingle3 = single [1,2,3,4,5] == False
+
+split :: [a] -> [[a]]
 split xs = [take n xs, drop n xs]
   where n = div (length xs) 2
+
+okSplit1 = split "abc" == ["a", "bc"]  
+okSplit2 = split "abcd" == ["ab", "cd"]  
   
+isegs :: [a] -> [[a]]
 isegs xs = [init xs, tail xs]
+
+okIsegs = isegs "abc" == ["ab", "bc"]
+
+minors :: [a] -> [[a]]
+minors [x, y] = [[x], [y]]
+minors (x : xs) = map (x :) (minors xs) ++ [xs]
+
+okMinors = (minors "abc" == ["ab", "ac", "bc"])
 
 recover :: [[a]] -> [a]
 recover xss = head (head xss) : last xss
+
+okRI = (recover (isegs "abcd") == (id "abcd"))
+
+allTests = [okSingle1, okSingle2, okSingle3, okSplit1, okSplit2, okIsegs, okMinors]
+testAll = (M.getAll $ M.mconcat $ map M.All allTests) == True
